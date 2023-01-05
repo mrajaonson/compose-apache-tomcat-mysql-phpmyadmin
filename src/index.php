4dfa0e4bd6b892b977cba30936d2a0b8e6c26bf8
mrajaonson <m.rajaonson@posteo.net>
2023-01-05 18:28:27 +0100

    Add list files

--- src/index.php
+++ src/index.php
@@ -1,3 +1,28 @@
 <?php
-phpinfo();
+// Définit le répertoire source
+$source_dir = './';
+
+// Ouvre le répertoire source
+$dir = opendir($source_dir);
+
+// Code html du lien
+function lien($url)
+{
+    return "<a href=\"$url\">$url</a>";
+}
+
+// Lit chaque entrée du répertoire
+while (($entry = readdir($dir)) !== false) {
+    // Ignore les entrées "." et ".."
+    if ($entry == '.' || $entry == '..') {
+        continue;
+    }
+
+    // Affiche l'entrée
+    echo "<li>" . lien($entry) . "<br></li>";
+
+}
+
+// Ferme le répertoire
+closedir($dir);
 ?>
